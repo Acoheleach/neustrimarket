d78b6641add78be4ba5468ed1f0566bd5306e27b
Crisppy <47862756+Crisppy@users.noreply.github.com>
2019-02-25 22:59:35 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-let prefix = "!"
+let prefix = "!!"
 
 client.on('ready', function () {
   console.log("Je suis connecté !");
@@ -13,79 +13,196 @@ client.login(process.env.TOKEN);
 client.on("message", message => {
     if (!message.guild) return
     let args = message.content.trim().split(/ +/g)
-    if (args[0].toLowerCase() === prefix + "vc") {
-        let quantity = args[1];
-        let items = args[2];
-        let paiement = args[3];
-        
-        if (!quantity,!items,!paiement) return message.channel.send("**:gear: Veuillez respectez l'écriture, [quantité] [Type_D'objet] [Moyen de paiement].**")return
-        if (quantity,items,paiement) return message.channel.send(`**:gear: Bonjour ${message.author.username}, Vous souhaitez commander ${quantity} de ${items} et payer en ${paiement}. Est-ce correcte ?**`)
-        .then(message => {
-            message.react('✅')
-            message.react('❌')
-              // on attend l'event d'ajout d'une réaction
-              client.on('messageReactionAdd', (reaction, user) => {
 
-                if (reaction.emoji.name === '✅' && user.id !== client.user.id) {
-                  let namec = user
-                  let dm = user.id
-                  message.channel.bulkDelete(parseInt(0) + 2)
-                  var gro = client.channels.get('525351149952565248');
-                  gro.send(`**:gear: ${namec} Votre commande a été prise en compte, vous serez notifié quand un membre de la faction répondra à votre demande**`)
-                    var robby = client.channels.get('546354572701073419');
-                    robby.send(`**:gear:@here Le joueur ${namec} souhaite commander ${quantity} ${items} et payer en ${paiement}. \n🛑Utilisez pour refuser une commande, 👋 pour signalez que vous préparez la commande et 🍾 pour signalez que vous avez terminez la commande**`)
-
-                    .then(message => {
-                      message.react('🍾')
-                      message.react('🛑')
-                      message.react('👋')
-                        // on attend l'event d'ajout d'une réaction
-                        client.on('messageReactionAdd', (reaction, user) => {
-          
-                          if (reaction.emoji.name === '🍾' && user.id !== client.user.id) {
-                            client.users.get(dm).send("**:gear: Votre commande est terminé. Veuillez contacter un membre de la faction pour l'échange.**")
-                            message.channel.bulkDelete(parseInt(0) + 1)
-                          }
-
-                          if (reaction.emoji.name === '👋' && user.id !== client.user.id) {
-                            client.users.get(dm).send("**:gear: Votre commande est en préparation.**")
-                          }
-                          
-                          if (reaction.emoji.name === '🛑' && user.id !== client.user.id) {
-                              client.users.get(dm).send("**:gear: Malheureusement votre commande a été refusé. Pour avoir avantage d'explication nous vous invitons à nous contacter sur notre discord.**")
-                              message.channel.bulkDelete(parseInt(0) + 1)return
-                          }
-                          
-
-                        })
-
-                      })
-                
-
-                }
-
-
-                if (reaction.emoji.name === '❌' && user.id !== client.user.id) {
-                    message.channel.bulkDelete(parseInt(1) + 1)
-                }
-              })
-            })
-    }
-
-    if (args[0].toLowerCase() === prefix + "marketclear") {
+      if (args[0].toLowerCase() === prefix + "marketclear") {
         if (!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send("Vous n'avez pas la permission d'utiliser cette commande")
         let count = args[1]
         if (!count) return message.channel.send("Veuillez indiquer un nombre de messages à supprimer")
         if (isNaN(count)) return message.channel.send("Veuillez indiquer un nombre valide")
         if (count < 1 || count > 100) return message.channel.send("Veuillez indiquer un nombre entre 1 et 100")
         message.channel.bulkDelete(parseInt(count) + 1)
-    }
+      }
 
-    if (args[0].toLowerCase() === prefix + "marketinfo") {
+      if (args[0].toLowerCase() === prefix + "info") {
         if (!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send("Vous n'avez pas la permission d'utiliser cette commande")
         let count = 0
         message.channel.bulkDelete(parseInt(count) + 1)
-        message.channel.send("**:gear: @here Ici, c'est la boutique vous pouvez nous passer commande, nous essayerons de répondre à vos besoins le plus rapidement possible avec un prix convenable :moneybag: \nSi vous souhaitez commander ici veuillez faire la commande suivante:  !vc [Quantité] [Objet de la commande] [Moyen de paiement]**\n*:warning: Attention si vos réponses contiennent des espaces remplacer les par _ comme dans l'exemple ci-dessous*\n**!vc 2 dc Hell sickle vanadium :x: \n!vc 2_dc Hell_Sickle Vanadium :white_check_mark:  **")}
+        message.channel.send({embed: {
+          color: 23983,
+          author: {
+            name: client.user.username,
+            icon_url: client.user.avatarURL
+          },
+          title: '**:large_orange_diamond: Informations sur le Bot :large_blue_diamond:**',
+          description: '',
+          fields:[{
+            name: ':moneybag: ***Infomation sur les commandes***:moneybag:',
+            value: 'Baise le cul de balisto'
+          },
+          {
+            name: ':notes: ***Infomation sur les musiques***:notes: ',
+            value: 'Baise le cul de Ender'
+          },
+          {
+            name: ':wrench:***Information sur la modération***:wrench:',
+            value: 'Baise le cul de Never'
+          }],
+          timestamp: new Date(),
+          footer: {
+            icon_url: client.user.avatarURL,
+            text: 'Par Le Bot De La Faction Neustria',
+          }
+        }});   
+      }
 
+    if (args[0].toLowerCase() === prefix + "vc") {
+        let gcounter = 0
+        let quantity = args[1];
+        let items = args[2];
+        let paiement = args[3];
+        let namec = message.author.username;
+        let dm = message.author.id;
+        
+        if (!quantity,!items,!paiement){
+          message.channel.send({embed: {
+            color: 9830405,
+            author: {
+              name: client.user.username,
+              icon_url: client.user.avatarURL
+            },
+            title: '**:gear:Erreur sur la commande**',
+            description: '',
+            fields:[{
+              name: '***Veuillez respectez l\'écriture de la commande.***',
+              value: '!vc [Quantité] [Objet de la commande] [Moyen de paiement]\nVous pouvez aussi utiliser les émoticones correspondant à votre commande'
+            },
+            {
+              name: '***:warning: Attention si vos réponses contiennent des espaces remplacer les par _ comme dans l\'exemple ci-dessous***',
+              value: '!vc 2 dc Hell sickle vanadium :x: \n!vc 2_dc Hell_Sickle Vanadium :white_check_mark:'
+            }],
+            timestamp: new Date(),
+            footer: {
+              icon_url: client.user.avatarURL,
+              text: 'Par ⚡NeustriaBot⚡ || By Crisppy 💚',
+            }
+          }});
+        }
+        if (quantity,items,paiement) {
+          var gro = client.channels.get('525351149952565248');
+          message.channel.bulkDelete(parseInt(0) + 1)        
+                  gro.send({embed: {
+                    color: 1547008,
+                    author: {
+                      name: client.user.username,
+                      icon_url: client.user.avatarURL
+                    },
+                    title: '**:gear:Confirmation de la commande**',
+                    description: '',
+                    fields:[{
+                      name: `***Bonjour ${message.author.username}, votre commande de ${quantity} ${items} est prise en compte***`,
+                      value: 'Si il y a un problème dans votre commande, merci d\'avertir un administrateur rapidement'
+                    }],
+                    timestamp: new Date(),
+                    footer: {
+                      icon_url: client.user.avatarURL,
+                      text: 'Par ⚡NeustriaBot⚡ || By Crisppy 💚',
+                    }
+                  }});
+                    var robby = client.channels.get('546354572701073419');
+                    gcounter++;
+                    robby.send({embed: {
+                      color: 23983,
+                      author: {
+                        name: client.user.username,
+                        icon_url: client.user.avatarURL
+                      },
+                      title: '**Demande de commande n°**'+ gcounter,
+                      description: '',
+                      fields:[{
+                        name: `***Bonjour ${message.author.username}, Souhaite commander ${quantity} ${items} et son moyen de paiement est le ${paiement}.***`,
+                        value: 'Les réactions suivantes servent à accepter,refuser ou prévenir de la préparation d\'une commande'
+                      }],
+                      timestamp: new Date(),
+                      footer: {
+                        icon_url: client.user.avatarURL,
+                        text: 'Par ⚡NeustriaBot⚡ || By Crisppy 💚',
+                      }
+                    }})
+                    .then(message => {
+                      message.react('✔')
+                      message.react('❌')
+                      message.react('🕑')
+                      // on attend l'event d'ajout d'une réaction
+                      client.on('messageReactionAdd', (reaction, user) => {
+                        if (reaction.emoji.name === '✔' && user.id !== client.user.id) {
+                            client.users.get(dm).send({embed: {
+                              color: 1547008,
+                              author: {
+                                name: client.user.username,
+                                icon_url: client.user.avatarURL
+                              },
+                              title: '**Validation de votre commande.**',
+                              description: '',
+                              fields:[{
+                                name: `***Bonjour ${namec}, j'ai le plaisir de vous annoncez que votre commande de ${quantity} ${items} est terminé.***`,
+                                value: 'Pour procéder au paiement, veuillez contactez un administrateur ou un modérateur de la faction\n:warning: Vous avez 48 heures après récéption de ce message pour contacter un responsable de la faction,le cas échéant votre commande sera annulé'
+                              }],
+                              timestamp: new Date(),
+                              footer: {
+                                icon_url: client.user.avatarURL,
+                                text: 'Par ⚡NeustriaBot⚡ || By Crisppy 💚',
+                              }
+                            }});
+                            message.channel.bulkDelete(parseInt(0) + 1)
+                          }
 
-})
+                        if (reaction.emoji.name === '🕑' && user.id !== client.user.id) {
+                            client.users.get(dm).send({embed: {
+                              color: 23983,
+                              author: {
+                                name: client.user.username,
+                                icon_url: client.user.avatarURL
+                              },
+                              title: '**Commande en cours.**',
+                              description: '',
+                              fields:[{
+                                name: `***Bonjour ${namec}, j'ai le plaisir de vous annoncez que votre commande de ${quantity} ${items} est en cours de préparation.***`,
+                                value: 'Je vous recontacterai pour vous avertir lorsque votre commande sera terminé'
+                              }],
+                              timestamp: new Date(),
+                              footer: {
+                                icon_url: client.user.avatarURL,
+                                text: 'Par ⚡NeustriaBot⚡ || By Crisppy 💚',
+                              }
+                            }});
+                          }
+                          
+                        if (reaction.emoji.name === '❌' && user.id !== client.user.id) {
+                            client.users.get(dm).send({embed: {
+                              color: 9830405,
+                              author: {
+                                name: client.user.username,
+                                icon_url: client.user.avatarURL
+                              },
+                              title: '**Refus de votre commande.**',
+                              description: '',
+                              fields:[{
+                                name: `***Bonjour ${namec}, j'ai le malheur de vous annoncez que votre commande de ${quantity} ${items} est refusé.***`,
+                                value: 'Pour avoir de plus amples infomation suite à votre refus veuillez contactez un administrateur ou un modérateur de la faction'
+                              }],
+                              timestamp: new Date(),
+                              footer: {
+                                icon_url: client.user.avatarURL,
+                                text: 'Par ⚡NeustriaBot⚡ || By Crisppy 💚',
+                              }
+                            }});
+                            message.channel.bulkDelete(parseInt(0) + 1)
+                          }
+                          
+
+                        })
+
+                      })
+                    }
+                  }
+              })
